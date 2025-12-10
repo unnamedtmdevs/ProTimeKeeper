9de6e304c09613500b37aea87169f8403471c7fd
ludik <bakhanetssimon@gmail.com>
2025-12-10 16:10:56 +0300

    Initial Commit

---
+++ ProTime Keeper/ProTime_KeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProTime_KeeperApp.swift
+//  ProTime Keeper
+//
+//  Created by Simon Bakhanets on 10.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct ProTime_KeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ProTime KeeperTests/ProTime_KeeperTests.swift
@@ -0,0 +1,16 @@
+//
+//  ProTime_KeeperTests.swift
+//  ProTime KeeperTests
+//
+//  Created by Simon Bakhanets on 10.12.2025.
+//
+
+import Testing
+
+struct ProTime_KeeperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
